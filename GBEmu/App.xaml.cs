f6e8a1162a21dda39afaca4ebe3f5fdd188390d9
Silenthal Makram <silenthal.makram@gmail.com>
2020-04-04 00:47:50 -0400

    Use NuGet for the SharpDX libraries

--- GBEmu/App.xaml.cs
+++ GBEmu/App.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Reflection;
-using System.Windows;
+﻿using System.Windows;
 
 namespace GBEmu
 {
@@ -10,19 +7,5 @@ namespace GBEmu
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler((sender, args) =>
-            {
-                string folderPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-                string assemblyPath = Path.Combine(folderPath, "Lib", new AssemblyName(args.Name).Name + ".dll");
-                if (File.Exists(assemblyPath) == false)
-                {
-                    return null;
-                }
-                return Assembly.LoadFrom(assemblyPath);
-            });
-        }
     }
 }
